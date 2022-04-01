c3cb73dc4f1e32d14cb114125caf08cff65939c5
Devan <devan_fast@hotmail.com>
2022-04-01 11:04:17 -0700

    Add files via upload

---
+++ AutoClick RecManagement Web.js
@@ -0,0 +1,26 @@
+console.log("Custom Script");
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const records= urlParams.get('records');
+
+if(records == 1){
+    console.log("#####Running Records Management script here#####");
+    
+    window.onload = function(){
+        document.getElementsByClassName("inputMultipleSelectOptionTitle")[11].click()
+        document.getElementsByClassName("inputMultipleSelectOptionTitle")[13].click()
+        document.getElementById("button_continue").click()
+      };
+}
+if(records == 2){
+    console.log("#####Running Records Management script here#####");
+      
+    window.onload = function(){
+        document.getElementsByClassName("inputMultipleSelectOptionTitle")[11].click()
+        document.getElementsByClassName("inputMultipleSelectOptionTitle")[14].click()
+        document.getElementById("button_continue").click()
+      };
+}
+// outputs two direct access links
+// build these links into a seperate page on our Wordpress intranet 
