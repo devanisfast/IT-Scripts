8a98fe79e37c5761c0bdce93483cb2ea3059f531
Devan <devan_fast@hotmail.com>
2022-08-05 08:36:43 -0700

    Add files via upload

---
+++ appdata-roaming-laptop-transfer.ps1
@@ -0,0 +1,12 @@
+﻿#Appdata Roaming Transfers
+
+$PCnum = Read-Host -Prompt 'Enter the old PC number'
+$userName = Read-Host -Prompt 'Enter username'
+$PCdest = Read-Host -Prompt 'Enter the new PC number'
+
+$exclude = @("*.crl","*.log","*.bin","*.jfm","*.vol","*.edb","*.jcp","*.jtx","*.jrs","*.ost","*.nst","*.pst","*.exe")
+$sourceFolder = '\\'+$PCnum+'\c$\Users\'+$userName+'\appdata\Roaming\'
+$destination  = '\\'+$PCdest+'\c$\Users\'+$userName+'\appdata\Roaming\'
+
+New-Item -ItemType Directory -Force -Path $destination
+Get-ChildItem $sourceFolder -Exclude $exclude | Copy-Item -Destination $destination -Exclude $exclude -Recurse -Container -Force -ErrorAction SilentlyContinue
