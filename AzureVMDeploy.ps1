51aa9ca67ba1f59a6a561fbbc9b0dd17e21bdd1e
Devan <devan_fast@hotmail.com>
2023-07-13 09:31:31 -0700

    Update AzureVMDeploy.ps1

--- AzureVMDeploy.ps1
+++ AzureVMDeploy.ps1
@@ -1,6 +1,6 @@
 #every item is contingent on the vm-name
 #ensure that it is all lowercase and less than 15 characters
-#for domainUser and domainPassword enter your admin credentials eg. dfast.admin and pwd
+#for domainUser and domainPassword enter your admin credentials eg. user.admin and pwd
 Connect-AzAccount
 Write-Output "Ensure VMname is all lowercase < 15 characters, symbols only use - "
 $vmname = Read-Host "Enter Your VM Name"
